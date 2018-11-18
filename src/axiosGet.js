a08f3880940747ce130c27eb7ce7317a34d48202
mat <m.przybylowski@outlook.com>
2018-11-18 23:37:14 +0100

    change axios url

--- src/axiosGet.js
+++ src/axiosGet.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const axiosGet = axios.create({
-    baseURL: 'http://localhost:8888/data-buikakru/wp-json/wp/v2/posts?categories=2'
+    baseURL: 'http://data.buikakru.com/wp-json/wp/v2/posts?categories=2'
     });
 
 export default axiosGet;
